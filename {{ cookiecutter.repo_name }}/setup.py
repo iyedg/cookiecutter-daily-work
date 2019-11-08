a6bfb85089114a5ca34b3d86e56c18bbe1c35d35
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-11-08 21:08:18 +0100

    update setup.py with console entry point

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -6,5 +6,6 @@ setup(
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
+    entry_points={"console_scripts": ["{{ cookiecutter.project_name }}={{ cookiecutter.project_name }}.data.make_dataset:make"]},
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
 )
